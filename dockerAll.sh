aa2e202c478aeef181c32ed06a9d772e2c6e638f
Majed CHATTI (Ingenico Partner) <Majed.CHATTI@ingenico.com>
2018-11-14 11:35:52 +0100

    improve scripts

--- dockerAll.sh
+++ dockerAll.sh
@@ -1,29 +1,50 @@
 #!/bin/bash
 
+printHelper () {
+    echo 'Please provide a valid action ls/stop/rm'
+    exit 1
+}
+
 if [ $# -eq 0 ]
 then
-    echo 'Please provide action stop/rm'
-    exit 1
+    printHelper
+fi
+
+done="false"
+
+if [ "$1" == "ls" ]
+then
+    done="true"
+    docker ps -a
+    exit 0
 fi
 
 if [ "$1" == "stop" ]
 then
+    done="true"
     containers=$(docker ps -a -q)
     if [ -z "$containers" ]; then
         echo "there is no container to stop"
     else
         docker stop $containers
     fi
-else 
-    if [ "$1" == "rm" ]
-    then
-        containers=$(docker ps -a -q)
-        if [ -z "$containers" ]; then
-            echo "there is no container to remove"
-        else
-            docker rm $containers
-        fi
-    else
-        echo 'Please provide a valid action stop/rm'
-    fi
+    exit 0
 fi
+
+if [ "$1" == "rm" ]
+then
+    done="true"
+    containers=$(docker ps -a -q)
+    if [ -z "$containers" ]; then
+        echo "there is no container to remove"
+    else
+        docker rm $containers
+    fi
+    exit 0
+fi
+
+if [ "$done" == "false" ]
+then
+    printHelper
+fi
+
